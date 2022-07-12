eace3f6a2679598ff3a822ead78147f042c60059
cmathieu <kws_guru@yahoo.com>
2022-07-11 19:39:14 -0700

    implements dynamic binding of stories

--- stories/approach.py
+++ stories/approach.py
@@ -40,14 +40,6 @@ class earthApproach(storyBase):
         api.setCurrentBody("sedna")
 
 
-        """
-        solSystem.Dashboard.focusTab.setCurrentBodyFocusManually(solSystem.EarthRef, 2)
-        solSystem.introZoomIn(75) #38)
-        ###	solSystem.camera.cameraPanUp(1, 20)
-        ###	solSystem.camera.cameraLeft(1, 20)
-        solSystem.camera.cameraRotateDown(140)
-        """
-
 class jupiterApproach(storyBase):
 
     def play(self):
