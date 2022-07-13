ac59154f3b34b872a635479b030cefd5fa10be26
crmathieu <charles@gawkbox.com>
2022-07-13 08:24:06 -0700

    adds recording a story as an option at runtime during boot

--- orbital.py
+++ orbital.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
       else:
          storyObj = pclasses[pick_a_story(pclasses, None)]
 
-      recorder = raw_input("\nRecord story? (y/n) ")
+      recorder = raw_input("\nRecord story? (y/n)\n")
       
    # pass a story object to boot-loader (story is not instantiated yet)
    bootSolarSystem(storyObj, False if str(recorder).lower() == 'n' else True)
